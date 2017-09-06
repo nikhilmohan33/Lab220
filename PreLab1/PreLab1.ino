28e324cca58bbc19321ddfdd63894772679ee1af
msrasara <saram1@andrew.cmu.edu>
2017-09-06 19:24:14 -0400

    Add files via upload

---
+++ PreLab1/PreLab1.ino
@@ -0,0 +1,32 @@
+/*
+ * 18220 PreLab 1
+ */
+
+const int LED = 3;
+const int TRIGGER =2;
+int trigState = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
+  pinMode(TRIGGER, INPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  trigState =  digitalRead(TRIGGER);
+  if ( trigState == LOW) {
+    delay(500);
+    trigState =  digitalRead(TRIGGER);
+    if (trigState == HIGH){
+      digitalWrite(LED, HIGH);
+      delay(60);
+      digitalWrite(LED,LOW);
+      }
+  }
+  delay(30);
+}
+
+
